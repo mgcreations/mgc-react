91c8eabb7cf0102962e5dbbaa7e02cc6a45229e8
Michael Ashworth <info@mgcreations.co.za>
2020-04-04 08:47:25 +0200

    Updates

---
+++ precache-manifest.132dcff7357c019a60c27beeabb2103c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ccdfa62749f704a6e2047194b25792",
+    "url": "/mgc-react/index.html"
+  },
+  {
+    "revision": "0781a40c78d3ac792144",
+    "url": "/mgc-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "92242281e23e74266078",
+    "url": "/mgc-react/static/js/2.a0aa4d11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mgc-react/static/js/2.a0aa4d11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0781a40c78d3ac792144",
+    "url": "/mgc-react/static/js/main.a8abba9c.chunk.js"
+  },
+  {
+    "revision": "0f9c9e8e8513a92122ac",
+    "url": "/mgc-react/static/js/runtime-main.f843a264.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mgc-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
